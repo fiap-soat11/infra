b8b22ce37e8109dd89bca9a25ace162704c57bc8
asilvaribeiro <asilvaribeiro@prodam.sp.gov.br>
2025-08-21 16:53:28 -0300

    Estrutura de pastas repositorio

--- s3.tf
+++ s3.tf
@@ -1,7 +1,7 @@
 
 
-resource "aws_s3_bucket" "meu_bucket" {
-  bucket = "fiap-soat-11-teste" 
-  acl    = "private"
-  
-}
+#resource "aws_s3_bucket" "meu_bucket" {
+#  bucket = "fiap-soat-11-teste" 
+#  acl    = "private"
+#  
+#}
